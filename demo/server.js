0cfce2c2d7e1e22c66544f68942c05b630bdab89
Firede <firede@firede.us>
2014-04-26 15:42:47 +0800

    update dependencies.

--- demo/server.js
+++ demo/server.js
@@ -3,14 +3,12 @@ var stylus = require('stylus');
 var rider = require('../lib/rider');
 var ap = require('autoprefixer');
 var path = require('path');
-var http = require('http');
-var fs = require('fs');
 
 var app = express();
 
-function compile(str, path) {
+function compile(str, filePath) {
     return stylus(str)
-        .set('filename', path)
+        .set('filename', filePath)
         .set('resolve url', true)
         .use(rider())
         .on('end', prefixer);
@@ -20,12 +18,8 @@ function prefixer(err, css) {
     return ap('android >= 2.3', 'ios >= 5', 'ie 10').process(css).css;
 }
 
-app.set('port', process.env.PORT || 8848);
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
-app.use(express.logger('dev'));
-app.use(express.urlencoded());
-app.use(express.methodOverride());
 app.use(stylus.middleware({
     src: path.join(__dirname, 'public'),
     compile: compile
@@ -35,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/:page?', function (req, res) {
     var page = req.params.page || 'index';
     var viewPath = path.join(app.get('views'), page + '.ejs');
+    var fs = require('fs');
 
     if (fs.existsSync(viewPath)) {
         res.set('Content-Type', 'text/html');
@@ -45,6 +40,6 @@ app.get('/:page?', function (req, res) {
     }
 });
 
-http.createServer(app).listen(app.get('port'), function () {
-    console.log('Rider example started on port ' + app.get('port'));
+var server = app.listen(8888, function () {
+    console.log('Rider example started on port %d', server.address().port);
 });
